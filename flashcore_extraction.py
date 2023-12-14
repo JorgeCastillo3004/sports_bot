3ac1d7191e31187eb47ab8719551d5193b6fbc84
JorgeCastillo3004 <123377458+JorgeCastillo3004@users.noreply.github.com>
2023-12-13 23:59:16 -0400

    Add files via upload

--- flashcore_extraction.py
+++ flashcore_extraction.py
@@ -317,12 +317,16 @@ def get_news_info(driver, date):
 	save_image(driver, image_url, image_path)
 	mentions = get_mentions(driver)
 
-	dict_news = {'news_id':random_id()[0:], 'title':title[0:255], 'news_summary':summary.text[0:4196],
-				 'news_content':body_html[0:8392], 'image':image_path,
-				'published':date,'news_tags': mentions[0:255]}
+	dict_news = {'news_id':random_id(), 'title':title, 'news_summary':summary.text,
+				 'news_content':body_html, 'image':image_path,
+				'published':date,'news_tags': mentions}
 	
 	for key, field in dict_news.items():
 		print(key, len(str(field)), end='--')
+
+	dict_news = {'news_id':random_id(), 'title':title[0:255], 'news_summary':summary.text[0:4196],
+				 'news_content':body_html[0:8392], 'image':image_path,
+				'published':date,'news_tags': mentions[0:255]}
 
 	return dict_news
 
