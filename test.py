c5d0e0ac20deab5423736b952ad041e2ae733611
JorgeCastillo3004 <123377458+JorgeCastillo3004@users.noreply.github.com>
2023-12-19 02:20:12 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -10,55 +10,27 @@ def getdb():
         dbname='sports_db',
         )
 
-def save_news_database(dict_news):	
-	query = "INSERT INTO news VALUES(%(news_id)s, %(news_content)s, %(image)s,\
-			 %(published)s, %(news_summary)s, %(news_tags)s, %(title)s)"
-	cur = con.cursor()
-	cur.execute(query, dict_news)
-	con.commit
+def save_news_database(dict_news):      
+        query = "INSERT INTO news VALUES(%(news_id)s, %(news_content)s, %(image)s,\
+                         %(published)s, %(news_summary)s, %(news_tags)s, %(title)s)"
+        cur = con.cursor()
+        cur.execute(query, dict_news)
+        con.commit
 
 print("Connections stablished")
 
 con = getdb()
+dict_news = dict_news = {'news_id':"asd223ddsf13", 'title':"insert new news" ,'news_summary':"summary.text",\
+                                 'news_content':"body_html", 'image':"image_path",\
+                                'published':datetime.now(),'news_tags': "mentions"}     
+save_news_database(dict_news)
+
 query = "SELECT * FROM news;"
 cur = con.cursor()
 cur.execute(query)
+results = cur.fetchall()
+for result in results:
+    print(result)
 
 con.close()
-# save_news_database(con, dict_news)
 
-
-# con.close()
-
-
-
-# ALTER TABLE news
-# ALTER COLUMN news_content TYPE VARCHAR(12578);
-# COMMIT;
-
-# ALTER TABLE news
-# ALTER COLUMN news_summary TYPE VARCHAR(4196);
-# COMMIT;
-
-# ALTER TABLE news
-# ALTER COLUMN news_tags TYPE VARCHAR(255);
-# COMMIT;
-
-# ALTER TABLE news
-# ALTER COLUMN title TYPE VARCHAR(255);
-# COMMIT;
-
-# COMMIT;
-
-
-# create table news
-# (
-#     news_id      varchar(40) not null
-#         primary key,
-#     news_content varchar(8392),
-#     image        varchar(255),
-#     published    timestamp(6),
-#     news_summary varchar(4196),
-#     news_tags    varchar(255),
-#     title        varchar(255)
-# );
